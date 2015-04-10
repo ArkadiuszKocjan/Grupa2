0e4459c2f8584450ba6fbe89a58756a4215708cb
petrus <p.dankowski112@gmail.com>
2015-04-10 15:29:07 +0200

    wrong URL test

--- DataProviderUnitTests/Class1.cs
+++ DataProviderUnitTests/Class1.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DataProvider;
+using System.Net;
 
 namespace DataProviderUnitTests
 {
@@ -12,13 +13,23 @@ namespace DataProviderUnitTests
     [TestClass]
     public class DataProviderTest
     {
+        
         [TestMethod]
         public void TestDownloadPageContent()
         {
             DataProvider.DataProvider provider = new DataProvider.DataProvider();
             IEnumerable<string> tmp = provider.DownloadPageContent(@"https://www.google.pl/", false);
-            Assert.AreEqual(tmp.ToList<string>().Count > 0, true);
+            Assert.AreEqual(tmp.ToList<string>().Any(), true);
             Assert.AreEqual(tmp.ToList<string>()[0].Length > 0, true);
+        
+        }
+
+        [ExpectedException(typeof(WebException))]
+        [TestMethod]
+        public void TestWrongURL()
+        {
+            DataProvider.DataProvider provider = new DataProvider.DataProvider();
+            IEnumerable<string> tmp = provider.DownloadPageContent(@"https://wssssBww.google.pl/", false);
         }
     }
 }
