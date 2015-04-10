a58f1f623d2db714d29eff9ee13a8b526e60eb59
glacius12 <pawlus.maciek15@gmail.com>
2015-04-10 15:18:55 +0200

    Added Searcher engine and its tests

---
+++ GrepEngine/Searcher.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrepEngine
+{
+    public class Searcher
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="inputText"></param>
+        /// <param name="searchedString"></param>
+        /// <returns></returns>
+        public IEnumerable<string> FindLineWithString(IEnumerable<string> inputText,string searchedString)
+        {
+            if (inputText == null || searchedString == null)
+            {
+                return new List<string>();
+                
+            }
+            IEnumerable<string> results = inputText.Where(s => s.Contains(searchedString));
+            Logger.Logger.Log("FindLineWithString executed");
+            return results;
+            
+        }
+    }
+}
---
+++ GrepEngineUnitTests/SearcherTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GrepEngine;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GrepEngineUnitTests
+{
+    [TestClass]
+    public class SearcherTests
+    {
+        private List<string> _listString;
+        private List<string> _correctLines;
+        private List<string> _emptyList;
+        [TestInitialize]
+        public void LoadTextInput()
+        {
+            _listString = new List<string>();
+            _listString.Add("So, oft it chances in particular men");
+            _listString.Add("That for some vicious mole of nature in them,");
+            _listString.Add("As, in their birth--wherein they are not guilty, ");
+            _listString.Add("Since nature cannot choose his origin-- ");
+            _listString.Add("That for some viciousss mole of nature in them,");
+            _correctLines = new List<string>();
+            _correctLines.Add("That for some vicious mole of nature in them,");
+            _correctLines.Add("That for some viciousss mole of nature in them,");
+            _emptyList = new List<string>();
+
+        }
+        [TestMethod]
+        public void FindLineWithStringTest()
+        {
+            var searcher = new Searcher();
+            CollectionAssert.AreEqual(searcher.FindLineWithString(_listString, "mole of").ToList(), _correctLines);
+        }
+
+        [TestMethod]
+        public void EmptyFindLineWithStringTest()
+        {
+            var searcher = new Searcher();
+            CollectionAssert.AreEqual(searcher.FindLineWithString(_emptyList, "ala").ToList(), _emptyList);
+        }
+
+        [TestMethod]
+        public void IncorrectFindLineWithStringTest()
+        {
+            var searcher = new Searcher();
+            CollectionAssert.AreEqual(searcher.FindLineWithString(_listString, "alasad").ToList(), _emptyList);
+        }
+
+        /// <summary>
+        /// checks if FindLineWithString returns empty IEnumerable based on null input
+        /// </summary>
+        [TestMethod]
+        public void ShouldReturnEmptyIEnumerable()
+        {
+            var searcher = new Searcher();
+            CollectionAssert.AreEqual(searcher.FindLineWithString(null, null).ToList(), _emptyList);
+        }
+
+    }
+}
