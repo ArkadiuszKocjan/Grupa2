fd8a85ba60d6472d0c746cc0be27c2404ad53bf1
petrus <p.dankowski112@gmail.com>
2015-04-10 15:21:01 +0200

    unit test sprawdza długość tablicy przechowującej zawartość strony

--- DataProviderUnitTests/Class1.cs
+++ DataProviderUnitTests/Class1.cs
@@ -4,16 +4,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataProvider;
 
 namespace DataProviderUnitTests
 {
 
     [TestClass]
-    public class UnitTest1
+    public class DataProviderTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestDownloadPageContent()
         {
+            DataProvider.DataProvider provider = new DataProvider.DataProvider();
+            IEnumerable<string> tmp = provider.DownloadPageContent(@"https://www.google.pl/", false);
+            Assert.AreEqual(tmp.ToList<string>().Count > 0, true);
+            Assert.AreEqual(tmp.ToList<string>()[0].Length > 0, true);
         }
     }
 }
