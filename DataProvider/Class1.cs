38ee0d00ef2399fad742aae59d08d40d112b09ff
petrus <p.dankowski112@gmail.com>
2015-04-10 15:18:46 +0200

    implementacja dataprovidera

--- DataProvider/Class1.cs
+++ DataProvider/Class1.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Text.RegularExpressions;
 
 namespace DataProvider
 {
-    public class Class1
+    public class DataProvider
     {
+
+        public IEnumerable<string> DownloadPageContent(string url, bool regexMode) 
+        {
+            WebClient dowloader = new WebClient();
+            try
+            {
+                string content = dowloader.DownloadString(url);
+                if (regexMode)
+                    content = Regex.Replace(content, @"\<(.+?)\>", "");
+                return  content.Split('\n');
+            }
+            catch (WebException ex)
+            {
+                Logger.Logger.Log(ex.Message);
+                throw;
+            }
+        }
     }
 }
